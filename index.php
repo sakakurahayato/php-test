b001f7a064edd3bc0088ecbe3d8ea3233369fb5c
sakakuarahayato <sakakura@sakakurasokutonoMacBook-Pro.local>
2021-12-03 19:23:42 +0900

    first-commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<table>
+<?php
+for($i=1;$i<=10;$i++){
+  echo '<tr>';
+for($a=1;$a<=10;$b++){
+  echo '<td>'.$i*$a.'</td>';
+}
+echo '</tr>';
+}
+?>
+</table>
---
+++ test.php
@@ -0,0 +1,4 @@
+<?php
+$name=htmlspecialchars($_REQUEST['name'],ENT_QUOTES);
+print $name."さん登録ありがとうございます";
+?>
